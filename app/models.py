7dbe7e04e11a0b5cf24853417991772faf03bf56
Nevojt <nevojt@gmail.com>
2023-11-10 20:18:57 +0100

    created microservices chat

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
+
+
+class Socket(Base):
+    __tablename__ = 'socket'
+    
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    message = Column(String, nullable=False)
+    receiver_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    rooms = Column(String, ForeignKey('rooms.name_room', ondelete='CASCADE'), nullable=False)
+  
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
+    email = Column(String, nullable=False, unique=True)
+    user_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    avatar = Column(String, nullable=False, server_default='https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/content%20common%20chat/Avatar%20Desktop/avatar_default.jpg')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
