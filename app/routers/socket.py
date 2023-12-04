2fed8d68f9eaa3b50c2246d9d15bd52c8daa521c
Nevojt <nevojt@gmail.com>
2023-12-04 20:07:50 +0100

    reload

--- app/routers/socket.py
+++ app/routers/socket.py
@@ -80,7 +80,7 @@ async def websocket_endpoint(
                 # Отримання оновлених повідомлень
                 # messages = await fetch_last_messages(room, session)
                 for user_id, (connection, _, _, user_room) in manager.user_connections.items():
-                    await connection.send_json({"message": "Reply posted"})
+                    await connection.send_json({"message": "Reload"})
                     if user_room == room:
                         for message in messages:
                             await connection.send_text(message.model_dump_json())
