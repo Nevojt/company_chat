95ed29fc9a371485b2d18fe54e9d5f2f1ea8986c
Nevojt <nevojt@gmail.com>
2024-06-18 21:44:32 +0200

    bugfix to system message

--- app/settings/connection_manager.py
+++ app/settings/connection_manager.py
@@ -90,6 +90,9 @@ class ConnectionManager:
 
         if add_to_db:
             file_id = await self.add_all_to_database(file, message, rooms, receiver_id, id_return)
+            
+        if file_id is None:
+            file_id = 0
 
         socket_message = schemas.SocketModel(
             id=file_id,
