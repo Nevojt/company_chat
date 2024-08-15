421a695474b6f342f3f1b7560471695ba73ac9bb
Nevojt <nevojt@gmail.com>
2024-08-15 16:03:36 +0200

    update Sayory

--- app/AI/sayory.py
+++ app/AI/sayory.py
@@ -8,16 +8,27 @@ sayori_key=settings.openai_api_key
 client = AsyncOpenAI(
     api_key=sayori_key
 )
-instruction_1 = "Ти асистент в менеджері і твоє ім'я Sayory далі буде повідомлення від користувача:  "
-instruction_2 = " Твоя відповідь не повинна перевищувати 600 символів."
+
+instruction = "Ти асистент в мессінджері, твоє ім'я Sayory, відповідь не повинна перевищувати 600 символів."
+
+
 async def ask_to_gpt(ask_to_chat: str) -> str:
     try:
         chat_completion = await client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {
+                "role": "system",
+                "content": [
+                    {
+                    "type": "text",
+                    "text": instruction
+                    }
+                ]
+                },
+                {
                 "role": "user",
-                "content": instruction_1 + ask_to_chat + instruction_2,
+                "content": ask_to_chat,
                 }
             ],
             temperature=1,
