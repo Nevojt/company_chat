4b22ddcc03ade9de61a4c78e507f45dd47d8ad63
Nevojt <nevojt@gmail.com>
2024-11-15 21:20:29 +0100

    update type message

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,57 +1,61 @@
-from typing import Optional
-from pydantic import BaseModel, Field
-from typing import Annotated
+
+from pydantic import BaseModel, Field, UUID4, Strict
+from typing import Annotated, Optional
 from datetime import datetime
 
         
 
     
         
-class SocketModel(BaseModel):
+class ChatMessagesSchema(BaseModel):
     created_at: datetime
-    receiver_id: Optional[int] = None
-    id: int
+    receiver_id:  Annotated[UUID4, Strict(False)] = None
+    id: Annotated[UUID4, Strict(False)]
     message: Optional[str] = None
     fileUrl: Optional[str] = None
+    voiceUrl: Optional[str] = None
+    videoUrl: Optional[str] = None
     user_name: Optional[str] = "USER DELETE"
     avatar: Optional[str] = "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/inne/image/boy_1.webp"
     verified: Optional[bool] = None
     vote: int
-    id_return: Optional[int] = None
+    id_return: Optional[UUID4] = None
     edited: bool
     deleted: bool
-    room_id: Optional[int] = None
- 
- # Send message to chat
+    room_id: Annotated[UUID4, Strict(False)] = None
+
+
+# Send message to chat
 class WrappedSocketMessage(BaseModel):
-    message: SocketModel        
-        
-def wrap_message(socket_model_instance: SocketModel) -> WrappedSocketMessage:
-    return WrappedSocketMessage(message=socket_model_instance)
+    message: ChatMessagesSchema
+
+
+async def wrap_message(chat_model_instance: ChatMessagesSchema) -> WrappedSocketMessage:
+    return WrappedSocketMessage(message=chat_model_instance)
 
 
 # Update message in chat
 class WrappedUpdateMessage(BaseModel):
-    update: SocketModel        
+    update: ChatMessagesSchema
         
-def wrap_message_update(socket_model_update: SocketModel) -> WrappedUpdateMessage:
+def wrap_message_update(socket_model_update: ChatMessagesSchema) -> WrappedUpdateMessage:
     return WrappedUpdateMessage(update=socket_model_update)
 
 
-class SocketUpdate(BaseModel):
-    id: int
+class ChatUpdateMessage(BaseModel):
+    id: Annotated[UUID4, Strict(False)]
     message: str
     
-class SocketDelete(BaseModel):
-    id: int
+class ChatMessageDelete(BaseModel):
+    id: Annotated[UUID4, Strict(False)]
         
 class Token(BaseModel):
     access_token: str
     token_type: str
-    
+
 class TokenData(BaseModel):
-    id: Optional[int] = None
+    id: Annotated[UUID4, Strict(False)]
     
 class Vote(BaseModel):
-    message_id: int
+    message_id: Annotated[UUID4, Strict(False)]
     dir: Annotated[int, Field(strict=True, le=1)]
